790ec0074f5cf635d5f124bb4d42ff28890f4d24
vanajmoorthy <vanajmoorthy@gmail.com>
2020-02-07 05:23:53 +0530

    add second route

--- server.go
+++ server.go
@@ -9,7 +9,12 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1> Serve stuff yo </h1>")
 }
 
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1> This is just a second route </h1>")
+}
+
 func main() {
 	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/about", aboutHandler)
 	http.ListenAndServe(":8000", nil)
 }
